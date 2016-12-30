d2ffa36e747d464e76eff258d0184608cdf3f414
rnoth <moth@airmail.cc>
2016-12-30 11:43:40 -0800

    clean up things

--- file.c
+++ file.c
@@ -3,26 +3,39 @@
 
 #include "edna.h"
 
+extern void readfile	(State *, char *);
+extern void writefile	(State *);
+
 void
 readfile (State *st, char *filename)
 {
 	char *buf;
 	size_t bufsiz;
-	
-	st->file = fopen(filename, "r");
+
+	st->file = fopen(filename, "r+");
 	if (!st->file)
-		die();
+		die("fopen");
 
 	buf = malloc (sizeof *buf * LINESIZE);
 	bufsiz = LINESIZE;
 	while (!feof (st->file)) {
 		if (-1 == getline (&buf, &bufsiz, st->file))
 			continue;
-		st->pos->line = putline (st->pos->line, buf, bufsiz, 1);
-		++st->pos->lineno;
+		st->curline = putline (st->curline, buf, bufsiz, 1);
+		++st->lineno;
 	}
 	free (buf);
 
 	return;
 }
-	
+
+void
+writefile (State *st)
+{
+	rewind (st->file);
+	do {
+		fputs (st->curline->str, st->file);
+		st->curline = st->curline->next;
+	} while (st->curline);
+	return;
+}
