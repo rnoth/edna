dfe47bb8038b073bffa999f09523e72b39a41903
rnoth <moth@airmail.cc>
2016-12-29 17:54:29 -0800

    make the prompt and error behaviors configurable

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
 /* config.h - configuration for edna(1) */
 
-#define PROMPT ":"
+#define PROMPT	":"
+#define ERROR	"?\n"
 
 const Command commands[] = {
 	{ "",	nop },
+	{ "",	gotol },
 	{ "a",	append },
 	{ "c",	change },
 	{ "d",	delete },
@@ -11,6 +13,4 @@ const Command commands[] = {
 	{ "i",	insert },
 	{ "p",	print },
 	{ "q",	quit },
-	{ "-",	back },
-	{ "+",	forward },
 };
