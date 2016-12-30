9916ac181933a8809ef002659937222c8ef03988
rnoth <moth@airmail.cc>
2016-12-30 11:45:03 -0800

    add more flexibility to command definitions

--- config.h
+++ config.h
@@ -1,16 +1,26 @@
 /* config.h - configuration for edna(1) */
 
-#define PROMPT	":"
-#define ERROR	"?\n"
+/* settings */
+#define PROMPT ":"
+#define ERROR "?\n"
 
-const Command commands[] = {
-	{ "",	nop },
-	{ "",	gotol },
-	{ "a",	append },
-	{ "c",	change },
-	{ "d",	delete },
-	{ "h",	help },
-	{ "i",	insert },
-	{ "p",	print },
-	{ "q",	quit },
+/* advanced examples */
+//#define PROMPT "%ld:", st->lineno	/* have line numbers in prompt */
+//#define ERROR "%s\n", error		/* like ed's 'H', without the '?' */
+
+static const Command commands[] = {
+       /* name,	func,	mode */
+	{ "",	nop,	NULL },
+	{ "",	gotol,	NULL },
+//	{ "",	print,	NULL },	/* ed-like behavior */
+	{ "a",	insert,	"append" },
+	{ "c",	insert,	"change" },
+	{ "d",	delete,	NULL },
+	{ "h",	help,	NULL },
+	{ "i",	insert,	NULL },
+	{ "p",	print,	NULL },
+	{ "q",	quit,	NULL },
+	{ "w",	write,	NULL },
+	{ "wq", quit,	"write" },
+	{ "Q",  quit,	"force" },
 };
