c67679eb3f78befea567412b518364f6b36162b2
rnoth <moth@airmail.cc>
2016-12-30 18:16:26 -0800

    double space columns

--- config.h
+++ config.h
@@ -9,18 +9,18 @@
 //#define ERROR "%s\n", error		/* like ed's 'H', without the '?' */
 
 static const Command commands[] = {
-       /* name,	func,	mode */
-	{ "",	nop,	NULL },
-	{ "",	gotol,	NULL },
-//	{ "",	print,	NULL },	/* ed-like behavior */
-	{ "a",	insert,	"append" },
-	{ "c",	insert,	"change" },
-	{ "d",	delete,	NULL },
-	{ "h",	help,	NULL },
-	{ "i",	insert,	NULL },
-	{ "p",	print,	NULL },
-	{ "q",	quit,	NULL },
-	{ "w",	write,	NULL },
-	{ "wq", quit,	"write" },
-	{ "Q",  quit,	"force" },
+      /*  name,		func,		mode */
+	{ "",		nop,		NULL },
+	{ "",		gotol,		NULL },
+	{ "a",		insert,		"append" },
+	{ "c",		insert,		"change" },
+	{ "d",		delete,		NULL },
+	{ "f",		filename,	NULL, },
+	{ "h",		help,		NULL },
+	{ "i",		insert,		NULL },
+	{ "p",		print,		NULL },
+	{ "q",		quit,		NULL },
+	{ "w",		write,		NULL },
+	{ "wq",		quit,		"write" },
+	{ "Q", 		quit,		"force" },
 };
