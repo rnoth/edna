29e9a24699ae4da03dca9cce3985ad7f90eff427
rnoth <moth@airmail.cc>
2016-12-30 20:44:36 -0800

    improve the parsing logic

--- input.c
+++ input.c
@@ -22,15 +22,11 @@ readline (char **buf_ptr, size_t *bufsiz_ptr, char *prompt, ...)
 void
 parseline (char *buf, char *name, Arg *arg)
 {
-	char ch;
+	char ch, delim;
 	for (size_t i = 0; (ch = buf[i]) ;++i) {
 		char tmp[64];
 		int j = 0;
 		switch (buf[i]) {
-		case ' ':
-		case '\t':
-		case '\n':
-			break;
 		case '+':
 		case '-':
 			arg->rel = 1;
@@ -38,7 +34,7 @@ parseline (char *buf, char *name, Arg *arg)
 			goto num;
 			break;
 		default:
-			if (isdigit(ch)) {
+			if (isdigit(ch) && !arg->addr) {
 				arg->rel = 0;
 				goto num;
 			}
@@ -59,19 +55,31 @@ parseline (char *buf, char *name, Arg *arg)
 				tmp[j] = ch;
 			tmp[j] = 0; /* terminate */
 			arg->addr = strtol (tmp, NULL, 10);
-			--i; /* push back last read char */
+			--i;
 			break;
 		cmd:
-			for (j = 0; isalnum(ch = buf[i]); ++i, ++j)
+			for (j = 0; isalpha(ch = buf[i]); ++i, ++j)
 				name[j] = ch;
 			name[j] = 0; /* terminate */
-			--i; /* push back last read char */
+			if (isdigit(buf[i]))
+				goto arg;
+			--i;
+			break;
+		arg:
+			for (j = 0; !isblank (ch = buf[i]); ++i, ++j)
+				arg->str[j] = buf[i];
+			arg->str[j] = 0;
 			break;
 		str:
-			for (j = 0; (ch = buf[i]); ++i, ++j)
+			delim = buf[i];
+			++i;
+			for (j = 0; (ch = buf[i]) && ch != delim; ++i, ++j)
 				arg->str[j] = ch;
-			chomp (arg->str, j);
-			--i; /* push back last read char */
+			arg->str[j] = 0;
+			chomp (arg->str, j); /* only used here because only str
+					      * will read to the end of the buf
+					      */
+			--i;
 		}
 	}
 	return;
