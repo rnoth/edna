3497b1abc1b0cb8c503261a28c333821735dfa8a
rnoth <moth@airmail.cc>
2017-01-02 19:17:46 -0800

    add switchbuf command

--- cmd.h
+++ cmd.h
@@ -7,5 +7,6 @@ extern int	insert		(State *, Buffer *, Arg *, char *);
 extern int	nop		(State *, Buffer *, Arg *, char *);
 extern int	print		(State *, Buffer *, Arg *, char *);
 extern int	quit		(State *, Buffer *, Arg *, char *);
+extern int	switchbuf	(State *, Buffer *, Arg *, char *);
 extern int	write		(State *, Buffer *, Arg *, char *);
 
