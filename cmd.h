dc04eeb4cabfff86fba551d596c6fcfc20adf510
rnoth <moth@airmail.cc>
2017-01-02 20:51:36 -0800

    add some more file manipulation commands

--- cmd.h
+++ cmd.h
@@ -1,10 +1,13 @@
 /* commands.h -- command-specific header file */
+/* TODO: organization */
 extern int	delete		(State *, Buffer *, Arg *, char *);
+extern int	edit		(State *, Buffer *, Arg *, char *);
 extern int	filename	(State *, Buffer *, Arg *, char *);
 extern int	gotol		(State *, Buffer *, Arg *, char *);
 extern int	help		(State *, Buffer *, Arg *, char *);
 extern int	insert		(State *, Buffer *, Arg *, char *);
 extern int	nop		(State *, Buffer *, Arg *, char *);
+extern int	openbuf		(State *, Buffer *, Arg *, char *);
 extern int	print		(State *, Buffer *, Arg *, char *);
 extern int	quit		(State *, Buffer *, Arg *, char *);
 extern int	switchbuf	(State *, Buffer *, Arg *, char *);
