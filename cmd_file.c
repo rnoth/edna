72061454337a536eaa6d1357fe41b93867cf4801
rnoth <moth@airmail.cc>
2017-01-02 20:52:44 -0800

    edit(): edit new file or re-open current file

--- cmd_file.c
+++ cmd_file.c
@@ -5,9 +5,39 @@
 #include "edna.h"
 #include "cmd.h"
 
+extern int	edit		(State *, Buffer *, Arg *, char *);
 extern int	filename	(State *, Buffer *, Arg *, char *);
 extern int	quit		(State *, Buffer *, Arg *, char *);
 extern int	write		(State *, Buffer *, Arg *, char *);
+
+int
+edit (State *st, Buffer *buf, Arg *arg, char *error)
+{
+	if (arg->mode && !strcmp (arg->mode, "force"))
+		goto end;
+	if (buf->dirty) {
+		strcpy (error, "buffer has unsved changes");
+		return 1;
+	}
+
+end:
+	if (EOF == fclose (buf->file)) {
+		warn ("fclose");
+		return 1;
+	}
+
+	for (; buf->curline->next; buf->curline = buf->curline->next)
+		;
+	freelines (buf->curline, NULL);
+	buf->curline = makeline();
+	buf->lineno = 0;
+
+	if (arg->cnt)
+		strcpy (buf->filename, arg->vec[0]);
+
+	readbuf (buf);
+	return 0;
+}
 
 int
 filename (State *st, Buffer *buf, Arg *arg, char *error)
