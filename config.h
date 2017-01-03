501b2464b51bfa472818fabf13de4b67dcc78d8d
rnoth <moth@airmail.cc>
2017-01-02 20:53:01 -0800

    book-keeping

--- config.h
+++ config.h
@@ -10,17 +10,20 @@
 //#define PROMPT "%ld:", st->lineno	/* have line numbers in prompt */
 //#define ERROR "%s\n", error		/* like ed's 'H', without the '?' */
 
-static const Command commands[] = {
+static Command commands[] = {
       /*  name,		func,		mode */
 	{ "",		nop,		NULL },
 	{ "",		gotol,		NULL },
 	{ "a",		insert,		"append" },
 	{ "c",		insert,		"change" },
 	{ "d",		delete,		NULL },
+	{ "e",		edit,		NULL },
+	{ "E",		edit,		"force" },
 	{ "f",		filename,	NULL, },
 	{ "h",		help,		NULL },
 	{ "i",		insert,		NULL },
 	{ "next",	switchbuf,	"next" },
+	{ "o",		openbuf,	NULL },
 	{ "p",		print,		NULL },
 	{ "prev",	switchbuf,	"prev" },
 	{ "q",		quit,		NULL },
