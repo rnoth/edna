962a3edc73a4729168fe4b57fbe385cc7e78335a
rnoth <moth@airmail.cc>
2017-01-02 20:51:12 -0800

    routines for qsort and bsearch

--- util.c
+++ util.c
@@ -1,6 +1,9 @@
 /* util.c -- various functions */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "edna.h"
 
 extern void	warn	(char *);
 inline void	die	(char *);
@@ -23,7 +26,18 @@ die (char *s)
 	perror(s);
 	exit(1);
 }
-	
+
+int
+cmdcmp (const void *a, const void *b)
+{
+	return strcmp (((Command *)a)->name, ((Command *)b)->name);
+}
+
+int
+cmdchck (const void *a, const void *b)
+{
+	return strcmp (((char *)a), ((Command *)b)->name);
+}
 
 void
 warn (char *s)
