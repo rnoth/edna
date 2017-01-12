9f4ec51d3f2f550d8df91966ebf0c9cd5bb2fe30
rnoth <moth@airmail.cc>
2017-01-11 17:37:28 -0800

    this was actually suppose to be a header file

--- set.h
+++ set.h
@@ -1,47 +1,13 @@
 /* set.h -- set operations */
+#include "edna.h"
+
+typedef unsigned long long  subset;
 typedef unsigned long long *Set;
 
-Set
-setcomplement (Set A, size_t len)
-{
-	Set B;
-	B = malloc (sizeof *B * len);
-	if (!B) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
-		B[i] = ~A[i];
-	return B;
-}
-
-Set
-setdifference (Set A, Set B, size_t len)
-{
-	Set C;
-	C = malloc (sizeof *C * len);
-	if (!C) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
-		C[i] = A[i] ^ B[i];
-	return C;
-}
-
-Set
-setintersect (Set A, Set B, size_t len)
-{
-	Set C;
-	C = malloc (sizeof *C * len);
-	if (!C) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
-		C[i] = A[i] & B[i];
-	return C;
-}
-
-Set
-setunion (Set A, Set B, size_t len)
-{
-	Set C;
-	C = malloc (sizeof *C * len);
-	if (!C) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
-		C[i] = A[i] | B[i];
-	return C;
-}
+extern Set makeset		(Buffer *buf);
+extern Set setaddmemb		(Set A, size_t len, size_t memb);
+extern Set setcomplement	(Set A, size_t len);
+extern Set setdifference	(Set A, Set B, size_t len);
+extern Set setintersect		(Set A, Set B, size_t len);
+extern Set setunion		(Set A, Set B, size_t len);
 
