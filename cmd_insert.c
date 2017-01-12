668eb74d5a3ec27bd72a401c85cb8bd7c4880e05
rnoth <moth@airmail.cc>
2017-01-11 17:27:18 -0800

    massively simplify insert(), remove signal handling, for now

--- cmd_insert.c
+++ cmd_insert.c
@@ -1,80 +1,41 @@
 /* cmd_insert.c -- line insertion commands */
 #include <stdlib.h>
 #include <string.h>
-#include <setjmp.h>
-#include <signal.h>
 
 #include "edna.h"
 #include "cmd.h"
 
 extern int	insert		(State *, Buffer *, Arg *, char *);
 
-static void escape (int);
-static sigjmp_buf jmp;
-
-void
-escape (int sig)
-{
-	siglongjmp (jmp, 1);
-}
-
 int
 insert (State *st, Buffer *buf, Arg *arg, char *error)
 {
-	char *line;
-	int option;
-	size_t linelen;
-	struct sigaction act, oldact;
+	Line *new;
 
-	option = INSERT;
+	if (arg->addr && gotol(st, buf, arg, error))
+		return 1;
+	arg->addr = 0;
 
 	if (arg->mode) {
 		if (!strcmp (arg->mode, "insert")) {
-			; /* insert mode is redundant */
+			;
 		} else if (!strcmp (arg->mode, "append")) {
-			option = APPEND;
+			if (!buf->curline->next) {
+				new = makeline();
+				linklines (buf->curline, new);
+			}
+			++buf->lineno;
+			buf->curline = buf->curline->next;
 		} else if (!strcmp (arg->mode, "change")) {
-			option = CHANGE;
+			if (!delete (st, buf, arg, error))
+				return 1;
 		} else {
 			strcpy (error, "unknown option");
 			return 1;
 		}
 	}
 
-	if (arg->addr && gotol(st, buf, arg, error))
-		return 1;
-
-	if (buf->lineno == 0 || option == APPEND)
-		++buf->lineno;
-
-	buf->dirty = 1;
-
-	line = malloc (sizeof *line * LINESIZE);
-	linelen = LINESIZE;
-
-	if (sigsetjmp (jmp, 1))
-		goto end;
-	act.sa_handler = escape;
-	sigaction (SIGINT, &act, &oldact);
-	
-	for (;;) {
-		if (printf ("%ld|", buf->lineno) < 0) die ("printf");
-		readline (&line, &linelen, stdin, error);
-		if (!strcmp (line, ".\n"))
-			break;
-		if(!(buf->curline = putline (buf->curline, line, linelen, option))) {
-			free (line);
-			strcpy (error, "insertion failed");
-			return 1; /* error */
-		}
-		++buf->lineno;
-		option = APPEND;
-	}
-
-end:
-	sigaction (SIGINT, &oldact, NULL);
-	--buf->lineno;
-	free (line);
+	setmode (st, buf, "insert");
 	return 0;
 }
 
