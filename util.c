53b8280167fa928c27a3b29db9ca9ebdbd4dd724
rnoth <moth@airmail.cc>
2017-01-11 17:38:40 -0800

    define warn() and die() as macros, to remove false positives from make lint

--- util.c
+++ util.c
@@ -5,43 +5,15 @@
 
 #include "edna.h"
 
-extern void	warn	(char *);
-inline void	die	(char *);
 extern void	chomp	(char *, size_t);
 
 void
-chomp(char *line, size_t linelen)
+chomp (char *line, size_t linelen)
 {
 	char *p = line;
 	for (size_t i = 0; line[i] && i < linelen; ++i)
 		if (line[i] == '\n')
 			p = line + i;
 	*p = 0;
-	return;
-}
-
-void
-die (char *s)
-{
-	perror(s);
-	exit(1);
-}
-
-int
-cmdcmp (const void *a, const void *b)
-{
-	return strcmp (((Command *)a)->name, ((Command *)b)->name);
-}
-
-int
-cmdchck (const void *a, const void *b)
-{
-	return strcmp (((char *)a), ((Command *)b)->name);
-}
-
-void
-warn (char *s)
-{
-	perror(s);
 	return;
 }
