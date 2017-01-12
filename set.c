dc3a2aaaac604611e618425a162c9f388b8b6d73
rnoth <moth@airmail.cc>
2017-01-11 17:40:33 -0800

    set.h implementation

---
+++ set.c
@@ -0,0 +1,69 @@
+#include <limits.h>
+#include <stdlib.h>
+
+#include "edna.h"
+#include "set.h"
+
+Set
+makeset (Buffer *buf)
+{
+	Set A;
+	A = calloc (buf->len % (sizeof *A * CHAR_BIT), sizeof *A);
+	return A;
+}
+
+Set
+setaddmemb (Set A, size_t len, size_t memb)
+{
+	size_t i = sizeof *A * CHAR_BIT;
+	for (size_t j=0; j < len && memb > 0; ++j, memb -= i)
+		if (memb < i) {
+			A[j] &= 1 << memb;
+			return A;
+		}
+	return NULL;
+}
+Set
+setcomplement (Set A, size_t len)
+{
+	Set B;
+	B = malloc (sizeof *B * len);
+	if (!B) die ("malloc");
+	for (size_t i = 0; i < len; ++i)
+		B[i] = ~A[i];
+	return B;
+}
+
+Set
+setdifference (Set A, Set B, size_t len)
+{
+	Set C;
+	C = malloc (sizeof *C * len);
+	if (!C) die ("malloc");
+	for (size_t i = 0; i < len; ++i)
+		C[i] = A[i] ^ B[i];
+	return C;
+}
+
+Set
+setintersect (Set A, Set B, size_t len)
+{
+	Set C;
+	C = malloc (sizeof *C * len);
+	if (!C) die ("malloc");
+	for (size_t i = 0; i < len; ++i)
+		C[i] = A[i] & B[i];
+	return C;
+}
+
+Set
+setunion (Set A, Set B, size_t len)
+{
+	Set C;
+	C = malloc (sizeof *C * len);
+	if (!C) die ("malloc");
+	for (size_t i = 0; i < len; ++i)
+		C[i] = A[i] | B[i];
+	return C;
+}
+
