f9dd0cb03db42d85e6e1d0aeb7899da4125fc816
rnoth <moth@airmail.cc>
2017-01-11 17:34:00 -0800

    use String-based interface

--- input.c
+++ input.c
@@ -9,7 +9,7 @@
 #include "edna.h"
 
 extern int	parseline	(char *, size_t, Arg *);
-extern int	readline	(char **, size_t *, FILE *, char *);
+extern int	readline	(String *, FILE *, char *);
 
 static struct reply*	lexline		(char *, size_t);
 
@@ -42,7 +42,7 @@ lexline (char *line, size_t linelen)
 	for (; isspace (ch = line[i]); ++i)
 		;
 	/* line address */
-	for (; isdigit (ch = line[i]) || strchr ("+-,", ch); ++i, ++j)
+	for (; ((ch = line[i]) && isdigit (ch)) || strchr ("+-,", ch); ++i, ++j)
 		s[j] = ch;
 	s[j] = 0;
 	++j;
@@ -148,30 +148,35 @@ parseline (char *line, size_t linelen, Arg *arg)
 }
 
 int
-readline (char **line, size_t *len, FILE *f, char *error)
+readline (String *str, FILE *file, char *error)
 {
 #	define CHUNK 20
-	char *tmp;
 	size_t off = 0;
+
 	for (;;) {
-		if (!memset (*line + *len - 1, '\n', 1)) die ("memset");
-		if (!fgets (*line + off, *len - off, f)) {
-			if (!feof (f)) { /* only report non-eof errors */
+		if (!memset (str->v + (str->m - 1), '\n', 1)) die ("memset");
+
+		if (!fgets (str->v + off, str->m - off, file)) {
+			if (!feof (file)) { /* only report non-eof errors */
 				strcpy (error, "fgets: ");
 				strncpy (error + strlen (error), strerror (errno), LINESIZE);
 			}
 			return FAIL;
 		}
-		if ((*line)[*len - 1] == '\n') /* got all of line */
+
+		if (str->v[str->m - 1] == '\n') /* got all of line */
 			break;
-		if (!(tmp = realloc (*line, *len + CHUNK))) {
+
+		if (!resizestring (str, str->m + CHUNK)) {
 			strcpy (error, "error allocating buffer");
 			return FAIL; /* error */
 		}
-		*line = tmp;
-		off = *len - 1;
-		*len += CHUNK;
+
+		off = str->c - 1;
 	}
+
+	str->c = strlen (str->v);
+
 	return SUCC;
 }
 
