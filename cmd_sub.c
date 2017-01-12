6791a9c80be3b93433cc83e973e714e81bf1efa6
rnoth <moth@airmail.cc>
2017-01-11 17:27:47 -0800

    remove memory leaks

--- cmd_sub.c
+++ cmd_sub.c
@@ -6,7 +6,7 @@
 #include "cmd.h"
 
 int
-sub (State *st, Buffer *buf, Arg *arg, char *error)
+subst (State *st, Buffer *buf, Arg *arg, char *error)
 {
 	regex_t *re;
 	regmatch_t *m;
@@ -14,23 +14,25 @@ sub (State *st, Buffer *buf, Arg *arg, char *error)
 	size_t i, j;
 
 	if (!(re = malloc (sizeof *re))) die ("malloc");
-	if (!(m = malloc (sizeof *m))) die ("malloc");
 
 	if (regcomp (re, arg->vec[0], REG_NEWLINE)) {
 		strcpy (error, "error compiling regex");
+		free (re);
 		return 1;
 	}
 
+	if (!(m = malloc (sizeof *m))) die ("malloc");
+
 	if (regexec (re, buf->curline->str, 1, m, 0) == REG_NOMATCH) {
 		strcpy (error, "no matches");
-		return 1;
+		goto finally;
 	}
 
 	i = strlen (buf->curline->str + m->rm_eo);
 	if (m->rm_so + i + strlen (arg->vec[1])
 	    > buf->curline->len) {
 		strcpy (error, "too large a replacement (FIXME)");
-		return 1;
+		goto finally;
 	}
 
 	if (!(tail = malloc (i))) die ("malloc");
@@ -40,6 +42,13 @@ sub (State *st, Buffer *buf, Arg *arg, char *error)
 	memcpy (buf->curline->str + j, tail, i + 1);
 
 	regfree (re);
+	free (m);
+	free (tail);
 
 	return print (st, buf, arg, error);
+
+finally:
+	regfree (re);
+	free (m);
+	return 1;
 }
