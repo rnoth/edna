d2fb7385f5dc25c2cf3c496885612a5375765d4c
rnoth <moth@airmail.cc>
2017-01-11 17:29:33 -0800

    abstract modes, handle prompts properly

--- config.h
+++ config.h
@@ -1,18 +1,30 @@
 /* config.h - configuration for edna(1) */
+#include "edna.h"
 #include "cmd.h"
 
 /* settings */
-#define PROMPT "\r\033[K:"
-#define ERROR "?\n"
-#define FILENAME "/tmp/edna.hup"
+#define PROMPT		"\r\033[K:"
+#define INS_PROMPT	" \b"
+#define ERROR		"?\n"
+#define FILENAME	"/tmp/edna.hup"
 
 /* advanced examples */
 //#define PROMPT "%ld:", st->lineno	/* have line numbers in prompt */
+//#define INS_PROMPT "%ld|", buf->lineno	/* have line number in ins mode */
 //#define ERROR "%s\n", error		/* like ed's 'H', without the '?' */
 
+extern int cmdprompt (State *, Buffer *, Arg *);
+extern int insprompt (State *, Buffer *, Arg *);
+extern int cmderror  (State *, Buffer *, Arg *, char *);
+
+static const Mode modes[] = {
+       /* name,	       prompter,    parser,     evaluator,   handler, */
+	{ "command",   cmdprompt,   parseline,  evalcmd,     cmderror, },
+	{ "insert",    insprompt,   insparse,   insline,     inserror, },
+};
+
 static const Command commands[] = {
-      /*  name,		func,		mode */
-	{ "",		nop,		NULL },
+       /* name,		func,		mode */
 	{ "",		gotol,		NULL },
 	{ "a",		insert,		"append" },
 	{ "c",		insert,		"change" },
@@ -27,8 +39,32 @@ static const Command commands[] = {
 	{ "p",		print,		NULL },
 	{ "prev",	switchbuf,	"prev" },
 	{ "q",		quit,		NULL },
-	{ "s",		sub,		NULL },
+	{ "s",		subst,		NULL },
 	{ "w",		write,		NULL },
 	{ "wq",		quit,		"write" },
 	{ "Q", 		quit,		"force" },
 };
+
+/* functions */
+int
+cmdprompt (State *st, Buffer *buf, Arg *arg)
+{
+	if (printf (PROMPT) < 0) die ("printf");
+	return SUCC;
+}
+
+int
+insprompt (State *st, Buffer *buf, Arg *arg)
+{
+	if (printf (INS_PROMPT) < 0) die ("printf");
+	return SUCC;
+}
+
+int
+cmderror (State *st, Buffer *buf, Arg *arg, char *error)
+{
+	if (!strcmp (error, "quit"))
+		return FAIL;
+	if (printf (ERROR) < 0) die ("printf");
+	return SUCC;
+}
