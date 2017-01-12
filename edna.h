150c0dd55d1cdfdaf4a9fd047cc568673aa29bf6
rnoth <moth@airmail.cc>
2017-01-11 17:30:57 -0800

    reorganize

--- edna.h
+++ edna.h
@@ -3,138 +3,162 @@
 #define _edna_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <regex.h>
 
-#define LEN(X) (sizeof X / sizeof *X)
+#include "util.h"
+#include "vector.h"
+#include "str.h"
 
 #define LINESIZE (80)
 
 #define FAIL (0)
 #define SUCC (1)
 
-enum {
-	INSERT = -1, /* this needs to be fixed */
-	CHANGE,
-	APPEND,
-};
+typedef unsigned char	byte;
+typedef size_t		linenum;
 
-typedef struct Line Line;
-typedef struct Command Command;
-typedef struct Arg Arg;
-typedef struct Record Record;
-typedef struct Mark Mark;
-typedef struct Buffer Buffer;
-typedef struct Register Register;
-typedef struct Macro Macro;
-typedef struct State State;
+typedef struct Arg	Arg;
+typedef struct Buffer	Buffer;
+typedef struct Command	Command;
+typedef struct Line	Line;
+typedef struct Macro	Macro;
+typedef struct Mark	Mark;
+typedef struct Mode	Mode;
+typedef struct Record	Record;
+typedef struct Register	Register;
+typedef struct State	State;
 
 struct Arg {
-	unsigned char rel;	/* is address relative? */
-	int addr;
-	char *name;
-	char *mode;
-	size_t cnt;	/* argc equivalent */
-	char **vec;	/* argv equivalent */
+	byte	rel;	/* is address relative? */
+	int	addr;
+	char*	name;
+	char*	mode;
+
+	size_t	cnt;	/* argc equivalent */
+	char**	vec;	/* argv equivalent */
 };
 
 struct Buffer {
-	unsigned char dirty;
-	FILE *file;
-	char *filename;
-	Line *curline;
-	size_t lineno;
-	Mark *marks;
+	/* file info */
+	byte	dirty;
+	FILE*	file;
+	char*	filename;
+
+	/* line info */
+	size_t	len;
+	Line*	top;
+	Line*	bot;
+	Line*	curline;
+	linenum	lineno;
+
+	/* misc. info */
+	Mode*	mode;
+	Mark*	marks;
 };
 
 struct Command {
-	char *name;
-	int (*func)(State *, Buffer *, Arg *, char *);
-	char *mode;
+	char*	name;
+	int	(*func)	(State *, Buffer *, Arg *, char *);
+	char*	mode;
 };
 
 struct Line {
-	size_t len;
-	char *str;
-	Line *next;
-	Line *prev;
+	size_t	len;
+	char*	str;
+	Line*	next;
+	Line*	prev;
 };
 
 struct Macro {
-	char *name;	/* macro name, not command name */
-	Command cmd;
-	Arg arg;
+	char*	name;	/* macro name, not command name */
+	Command	cmd;
+	Arg	arg;
 };
 
 struct Mark {
-	char *name;
-	Line *l;
-	size_t lineno;
+	char*	name;
+	Line*	line;
+	linenum	lineno;
+};
+
+struct Mode {
+	char*	name;
+	int	(*prompt)  (State *, Buffer *, Arg *);
+	int	(*parse)   (char *, size_t, Arg *);
+	int	(*eval)    (State *, Buffer *, Arg *, char *);
+	int	(*error)   (State *, Buffer *, Arg *, char *);
 };
 
 struct Record {
-	unsigned char ok;
-	char *error;
-	Arg *arg;
-	void *diff;
+	byte	ok;
+	char*	error;
+	Arg*	arg;
+	void*	diff;
 };
 
 struct Register {
-	char *name;
-	char *str;
+	char*	name;
+	char*	str;
 };
 
 struct State {
-	Command *commands;
-	size_t cmdlen;
-
-	Buffer **buffers;
-	size_t buflen;
-	Buffer *curbuf;
-	size_t bufno;
-
-	Macro *macros;
-	Register *regs;
+	VECTOR (Command, cmds);
+	VECTOR (Buffer*, buffers);
+	VECTOR (Mode,	 modes);
+#if 0
+	Macro*		 macros;
+	Register*	 regs;
+#endif
 };
 
-/* defined in buffer.c */
+/* buffer.c */
 extern int	addbuf		(State *, Buffer *);
+extern int	checkoutbuf	(Buffer *, State *, size_t);
 extern Buffer*	makebuf		(char *);
 extern void	freebuf		(Buffer *);
-extern int	rmbuf		(State *, Buffer *);
+extern int	rmbuf		(State *, size_t);
+extern int	returnbuf	(Buffer *, State *);
 
-/* defined in command.c */
-extern int	evalcmd		(State *, Arg *, char *);
+/* command.c */
+extern int	cmdcmp		(const void *, const void *);
+extern int	cmdchck		(const void *, const void *);
+extern int	evalcmd		(State *, Buffer *, Arg *, char *);
 
-/* defined in file.c */
-extern void	readbuf		(Buffer *, char *);
-extern void	writebuf	(Buffer *);
+/* file.c */
+extern int	readbuf		(Buffer *, char *);
+extern int	writebuf	(Buffer *, char *);
 
-/* defined in init.c */
+/* init.c */
 extern void	freearg		(Arg *);
 extern void	freestate	(State *);
-extern void	initst		(State *);
+extern void	init		(State *);
 extern Arg*	makearg		(void);
 extern State*	makestate	(void);
 
-/* defined in input.c */
-extern int	readline	(char **, size_t *, FILE *, char *);
+/* input.c */
+extern int	readline	(String *, FILE *, char *);
 extern int	parseline	(char *, size_t, Arg *);
 
-/* defined in line.c */
-extern Line*	getnext		(Line *);
-extern Line*	getprev		(Line *);
-extern Line*	makeline	();
-extern Line*	putline		(Line *, char *, size_t, int option);
+/* insert.c */
+extern int	inserror	(State *, Buffer *, Arg *, char *);
+extern int	insparse	(char *, size_t, Arg *);
+extern int	insline		(State *, Buffer *, Arg *, char *);
+extern void	inshandle	(int);
+
+/* line.c */
+extern Line*	makeline	(void);
+extern Line*	putline		(Line *, char *, size_t);
 extern Line*	walk		(Line *, int, char *);
 
-/* defined in region.c */
+/* mode.c */
+extern int	setmode		(State *, Buffer *, char *);
+
+/* region.c */
 extern void	freelines	(Line *, Line *);
 extern void	linklines	(Line *, Line*);
 
-/* defined in util.c */
+/* util.c */
 extern void	chomp		(char *, size_t);
-extern int	cmdcmp		(const void *, const void *);
-extern int	cmdchck		(const void *, const void *);
-extern void	die		(char *);
-extern void	warn		(char *);
+
 #endif
