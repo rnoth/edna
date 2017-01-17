062a300d86be7e6c32e4a9b2737b22d0ad892153
rnoth <moth@airmail.cc>
2017-01-16 22:31:30 -0800

    Consistency.

--- tests/vector_test.c
+++ tests/vector_test.c
@@ -7,61 +7,49 @@
 int
 main ()
 {
-	puts ("Declaring an int vector");
+	printf ("\tDeclaring an int vector...");
 	VECTOR (int, intvec);
+	printf ("done.\n");
 
-	puts ("Allocating memory for vector of 8 ints");
+	printf ("\tAllocating memory for vector of 8 ints...");
 	MAKE_VECTOR (int, intvec, 8);
 	assert (intvec.v[0] == 0);
-	puts (". ");
 	assert (intvec.c == 0);
-	puts (". ");
 	assert (intvec.m == 8);
-	puts (". done");
+	printf ("done.\n");
 
-	puts ("Testing append");
+	printf ("\tTesting append...");
 	VEC_APPEND (int, intvec, 2);
 	assert (intvec.v[0] == 2);
-	puts (". ");
 	VEC_APPEND (int, intvec, 5);
 	assert (intvec.v[1] == 5);
-	puts (". ");
 	assert (intvec.c == 2);
-	puts (". ");
 	assert (intvec.m == 8);
-	puts (". done");
+	printf ("done.\n");
 
-	puts ("Testing insert");
+	printf ("\tTesting insert...");
 	VEC_INSERT (int, intvec, 0, 4);
 	assert (intvec.v[0] == 4);
-	puts (". ");
 	assert (intvec.v[1] == 2);
-	puts (". ");
 	VEC_INSERT (int, intvec, 3, 1);
 	assert (intvec.v[3] == 1);
-	puts (". ");
 	assert (intvec.v[4] == 0);
-	puts (". ");
 	assert (intvec.v[0] == 4);
-	puts (". ");
 	assert (intvec.c == 4);
-	puts (". done");
+	printf ("done.\n");
 
-	puts ("Testing remove");
+	printf ("\tTesting remove...");
 	VEC_REMOVE (int, intvec, 2);
 	assert (intvec.v[2] == 1);
-	puts (". ");
 	assert (intvec.v[3] == 0);
-	puts (". ");
 	VEC_REMOVE (int, intvec, 2);
 	assert (intvec.v[2] == 0);
-	puts (". ");
 	VEC_REMOVE (int, intvec, 0);
 	assert (intvec.c == 1);
-	puts (". done");
+	printf ("done.\n");
 
-	puts ("Testing free");
+	printf ("\tTesting free...");
 	FREE_VECTOR (intvec);
-	puts("Testing complete. Everything went ok, looks like you didn't break anything");
+	printf ("done.\n");
 	return 0;
 }
