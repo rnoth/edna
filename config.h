bff0817564f8a98f5b03c93d4a8516e3ed8991bf
rnoth <moth@airmail.cc>
2017-01-16 22:25:43 -0800

    default addresses

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 
 /* settings */
 #define PROMPT		"\r\033[K:"
+#define PRINT_FMT	"%s", (*arg->sel.v)->str
 #define INS_PROMPT	" \b"
 #define ERROR		"?\n"
 #define FILENAME	"/tmp/edna.hup"
@@ -11,11 +12,13 @@
 /* advanced examples */
 //#define PROMPT "%ld:", st->lineno	/* have line numbers in prompt */
 //#define INS_PROMPT "%ld|", buf->lineno	/* have line number in ins mode */
+//#define PRINT_FMT	"%ld\t%s", buf->lineno, buf->curline->str /* like ed's `n' */
 //#define ERROR "%s\n", error		/* like ed's 'H', without the '?' */
 
 extern int cmdprompt (State *, Buffer *, Arg *);
-extern int insprompt (State *, Buffer *, Arg *);
 extern int cmderror  (State *, Buffer *, Arg *, char *);
+extern int insprompt (State *, Buffer *, Arg *);
+extern int print     (State *, Buffer *, Arg *, char *);
 
 static const Mode modes[] = {
        /* name,	       prompter,    parser,     evaluator,   handler, */
@@ -24,25 +27,25 @@ static const Mode modes[] = {
 };
 
 static const Command commands[] = {
-       /* name,		func,		mode */
-	{ "",		gotol,		NULL },
-	{ "a",		insert,		"append" },
-	{ "c",		insert,		"change" },
-	{ "d",		delete,		NULL },
-	{ "e",		edit,		NULL },
-	{ "E",		edit,		"force" },
-	{ "f",		filename,	NULL, },
-	{ "h",		help,		NULL },
-	{ "i",		insert,		NULL },
-	{ "next",	switchbuf,	"next" },
-	{ "o",		openbuf,	NULL },
-	{ "p",		print,		NULL },
-	{ "prev",	switchbuf,	"prev" },
-	{ "q",		quit,		NULL },
-	{ "s",		subst,		NULL },
-	{ "w",		write,		NULL },
-	{ "wq",		quit,		"write" },
-	{ "Q", 		quit,		"force" },
+       /* name,		func,		mode		defaddr */
+	{ "",		gotol,		NULL,		".", },
+	{ "a",		insert,		"append",	".", },
+	{ "c",		insert,		"change",	".", },
+	{ "d",		delete,		NULL,		".", },
+	{ "e",		edit,		NULL,		".", },
+	{ "E",		edit,		"force",	".", },
+	{ "f",		filename,	NULL,		".", },
+	{ "h",		help,		NULL,		".", },
+	{ "i",		insert,		NULL,		".", },
+	{ "next",	switchbuf,	"next",		".", },
+	{ "o",		openbuf,	NULL,		".", },
+	{ "p",		print,		NULL,		".", },
+	{ "prev",	switchbuf,	"prev",		".", },
+	{ "q",		quit,		NULL,		".", },
+	{ "s",		subst,		NULL,		".", },
+	{ "w",		write,		NULL,		"%", },
+	{ "wq",		quit,		"write",	"%", },
+	{ "Q", 		quit,		"force",	".", },
 };
 
 /* functions */
@@ -55,6 +58,15 @@ cmdprompt (State *st, Buffer *buf, Arg *arg)
 }
 
 int
+cmderror (State *st, Buffer *buf, Arg *arg, char *error)
+{
+	if (!strcmp (error, "quit"))
+		return FAIL;
+	if (printf (ERROR) < 0) die ("printf");
+	if (fflush (stdout) == EOF) warn ("fflush");
+	return SUCC;
+}
+int
 insprompt (State *st, Buffer *buf, Arg *arg)
 {
 	if (printf (INS_PROMPT) < 0) die ("printf");
@@ -63,11 +75,13 @@ insprompt (State *st, Buffer *buf, Arg *arg)
 }
 
 int
-cmderror (State *st, Buffer *buf, Arg *arg, char *error)
+print (State *st, Buffer *buf, Arg *arg, char *error)
 {
-	if (!strcmp (error, "quit"))
+	if (!buf->curline->str) {
+		strcpy (error, "empty buffer");
 		return FAIL;
-	if (printf (ERROR) < 0) die ("printf");
-	if (fflush (stdout) == EOF) warn ("fflush");
+	}
+
+	if (printf (PRINT_FMT) < 0) die ("printf");
 	return SUCC;
 }
