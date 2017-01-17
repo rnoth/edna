aa6d0a3ff1d625189be3a7b2db53f36a46800752
rnoth <moth@airmail.cc>
2017-01-17 13:56:53 -0800

    rename the archive

--- string.mk
+++ string.mk
@@ -1,8 +1,8 @@
-string.o: string.c
+str.o: string.c
 	@echo CC $(CLFAGS) -c -o $@ $<
 	@$(CC) $(CFLAGS) -c -o $@ $<
 
-string.a: string.o
+str.a: string.o
 	@echo AR $(ARFLAGS) $@ $<
 	@$(AR) $(ARFLAGS) $@ $< > /dev/null
 	@printf "\e[A\e[2K"
