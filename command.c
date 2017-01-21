b0911497aae990bda1d074490e308ac3af59c80e
rnoth <moth@airmail.cc>
2017-01-21 13:08:42 -0800

    command.c: basic signal handling

--- command.c
+++ command.c
@@ -1,4 +1,5 @@
 /* command.c -- interface for executing commands */
+#include <setjmp.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,10 +12,17 @@ static void	cleanup	(Buffer *, Arg *arg);
 extern int	cmdchck (const void *a, const void *b);
 extern int	cmdcmp (const void *a, const void *b);
 extern int	evalcmd	(State *st, Buffer *, String *, char *error);
+static void	sighandle (int);
+
+static sigjmp_buf	jbuf;
+static struct sigaction act, old;
 
 void
 cleanup (Buffer *buf, Arg *arg)
 {
+	/* reset signal handlers */
+	if (sigaction (SIGINT, &old, NULL) == -1)
+		perror ("sigaction");
 	/* free resources */
 	if (arg->mode)
 		free (arg->mode);
@@ -53,6 +61,16 @@ evalcmd (State *st, Buffer *buf, String *str, char *error)
 	if (!(arg = calloc (1, sizeof *arg))) die ("calloc");
 	if (!(arg->name = calloc (20, sizeof *arg->name))) die ("calloc");
 
+	act.sa_handler = sighandle;
+
+	if (sigaction (SIGINT, &act, &old) == -1) {
+		perror ("sigaction");
+		return (FAIL);
+	}
+
+	if (sigsetjmp (jbuf, 1))
+		goto finally;
+
 	if (parseline (str, buf, arg, error) == FAIL)
 		goto finally;
 
@@ -89,4 +107,13 @@ finally:
 	cleanup (buf, arg);
 	return ret;
 
+}
+
+void
+sighandle (int signo) {
+	switch (signo) {
+	case SIGINT:
+		siglongjmp (jbuf, 1);
+		break;
+	}
 }
