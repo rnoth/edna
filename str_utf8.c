9c3709e8a088211fce5742c352e5e7b82089c2cb
rnoth <moth@airmail.cc>
2017-01-21 21:20:35 -0800

    uchar_extent()

---
+++ str_utf8.c
@@ -0,0 +1,18 @@
+/* str_utf8.c -- utf8 conversion and parsing routines */
+#include <stdlib.h>
+#include "str.h"
+#include "util.h"
+
+int
+uchar_extent (unsigned char ch)
+{
+	if (ch >= 0xC2 && ch <= 0xDF)
+		return (2);
+	else if (ch >= 0xE0 && ch <= 0xEF)
+		return (3);
+	else if (ch >= 0xF0 && ch <= 0xF4)
+		return (4);
+	else
+		return (-1); /* error */
+}
+	
