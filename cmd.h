b1bfdc875aca2f3f8b551a6a9641a53e4632772b
rnoth <moth@airmail.cc>
2017-01-21 21:18:01 -0800

    migrate some declarations

--- cmd.h
+++ cmd.h
@@ -1,6 +1,35 @@
 /* commands.h -- command-specific header file */
 #ifndef _edna_cmds_
 #define _edna_cmds_
+
+#include "edna.h"
+
+typedef struct Arg	Arg;
+typedef struct Command	Command;
+
+struct Arg {
+	Selection  sel;
+	char*	   name;
+	char*	   mode;
+
+	size_t	   cnt;	/* argc equivalent */
+	char**	   vec;	/* argv equivalent */
+};
+
+struct Command {
+	char*	name;
+	int	(*func)	(State *, Buffer *, Arg *, char *);
+	char*	mode;
+	char*	defaddr;
+};
+
+/* command.c */
+extern int	cmdchck (const void *a, const void *b);
+extern int	cmdcmp (const void *a, const void *b);
+
+/* parse.c */
+extern int	parseline	(String *, Buffer *, Arg *, char *);
+
 /* TODO: organization */
 extern int	delete		(State *, Buffer *, Arg *, char *);
 extern int	edit		(State *, Buffer *, Arg *, char *);
