5acd271a54be265d14a132091b12921a4f238b47
rnoth <moth@airmail.cc>
2017-01-22 13:09:15 -0800

    str_utf8.c: generalize uchar_extent()

--- str_utf8.c
+++ str_utf8.c
@@ -1,12 +1,16 @@
 /* str_utf8.c -- utf8 conversion and parsing routines */
 #include <stdlib.h>
+#include <ctype.h>
+
 #include "str.h"
 #include "util.h"
 
 int
 uchar_extent (unsigned char ch)
 {
-	if (ch >= 0xC2 && ch <= 0xDF)
+	if (isascii (ch))
+		return (1);
+	else if (ch >= 0xC2 && ch <= 0xDF)
 		return (2);
 	else if (ch >= 0xE0 && ch <= 0xEF)
 		return (3);
