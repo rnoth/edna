3ef263d3e48f0b352ed0637b4e5548f42c9296da
rnoth <moth@airmail.cc>
2017-01-21 21:19:05 -0800

    utf-8 awareness, consistent interface

--- str.h
+++ str.h
@@ -3,16 +3,22 @@
 #define _string_
 
 typedef struct String	String;
+typedef unsigned long	rune;
 
 struct String {
-	size_t c; /* length */
-	char *v;  /* string */
+	size_t c; /* characters */
+	size_t b; /* bytes */
 	size_t m; /* allocated memory */
+	char *v;  /* string */
 };
 
-extern void	freestring	(String);
+/* string.c */
+extern void	freestring	(String *);
 extern String*	chartostr	(char *);
 extern int	copystring	(String *, String *);
-extern String	makestring	(size_t);
+extern String*	makestring	(size_t);
 extern int	resizestring	(String *, size_t);
+
+/* str_utf8.c */
+extern int	uchar_extent	(unsigned char);
 #endif
