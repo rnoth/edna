bdadb65e86b46d88facd07d5ca01d8c2220ee77c
rnoth <moth@airmail.cc>
2017-01-21 21:19:32 -0800

    actually work (I hope)

--- string.mk
+++ string.mk
@@ -1,8 +1,4 @@
-str.o: string.c
-	@echo CC $(CLFAGS) -c -o $@ $<
-	@$(CC) $(CFLAGS) -c -o $@ $<
-
-str.a: string.o
+str.a: string.o str_utf8.o
 	@echo AR $(ARFLAGS) $@ $<
 	@$(AR) $(ARFLAGS) $@ $< > /dev/null
 	@printf "\e[A\e[2K"
