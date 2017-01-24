3f5f921eb1d3a6117e70061b97fe8907b1a8ba6a
rnoth <moth@airmail.cc>
2017-01-23 19:34:12 -0800

    long longs aren't C89

--- set.h
+++ set.h
@@ -2,16 +2,17 @@
 #ifndef _set_
 #define _set_
 
-#include "edna.h"
+#define freeset(A) free (A);
 
-typedef unsigned long long  subset;
-typedef unsigned long long *Set;
+typedef unsigned long  subset;
+typedef unsigned long *Set;
 
-extern Set makeset		(Buffer *buf);
+extern Set makeset		(size_t len);
 extern Set setaddmemb		(Set A, size_t len, size_t memb);
 extern Set setcomplement	(Set A, size_t len);
 extern Set setdifference	(Set A, Set B, size_t len);
 extern Set setintersect		(Set A, Set B, size_t len);
+extern int setoffset		(Set A, Set B, size_t len);
 extern Set setunion		(Set A, Set B, size_t len);
 
 #endif
