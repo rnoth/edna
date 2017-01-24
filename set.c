2cd6b1eb267decfb12eb5f7e86446a2b206c5a07
rnoth <moth@airmail.cc>
2017-01-23 19:33:54 -0800

    C89 compatibility, a new function: setoffset()

--- set.c
+++ set.c
@@ -3,12 +3,13 @@
 
 #include "edna.h"
 #include "set.h"
+#include "util.h"
 
 Set
-makeset (Buffer *buf)
+makeset (size_t len)
 {
 	Set A;
-	A = calloc (buf->len % (sizeof *A * CHAR_BIT), sizeof *A);
+	A = calloc (len, sizeof *A);
 	return A;
 }
 
@@ -16,11 +17,14 @@ Set
 setaddmemb (Set A, size_t len, size_t memb)
 {
 	size_t i = sizeof *A * CHAR_BIT;
-	for (size_t j=0; j < len && memb > 0; ++j, memb -= i)
+	size_t j;
+
+	for (j = 0; j < len && memb > 0; ++j, memb -= i)
 		if (memb <= i) {
 			A[j] |= BIT (memb - 1);
 			return A;
 		}
+
 	return NULL;
 }
 
@@ -28,9 +32,12 @@ Set
 setcomplement (Set A, size_t len)
 {
 	Set B;
+	size_t i;
+
 	B = malloc (sizeof *B * len);
 	if (!B) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
+
+	for (i = 0; i < len; ++i)
 		B[i] = ~A[i];
 	return B;
 }
@@ -39,9 +46,12 @@ Set
 setdifference (Set A, Set B, size_t len)
 {
 	Set C;
+	size_t i;
+
 	C = malloc (sizeof *C * len);
 	if (!C) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
+
+	for (i = 0; i < len; ++i)
 		C[i] = A[i] ^ B[i];
 	return C;
 }
@@ -50,21 +60,51 @@ Set
 setintersect (Set A, Set B, size_t len)
 {
 	Set C;
+	size_t i;
+
 	C = malloc (sizeof *C * len);
 	if (!C) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
+
+	for (i = 0; i < len; ++i)
 		C[i] = A[i] & B[i];
+
 	return C;
+}
+
+int
+setoffset (Set A, Set B, size_t len)
+{
+	Set C, tmp; /* pointer to the last elem of B */
+	size_t off = 0;
+
+	for (tmp = B; *tmp; ++tmp)
+		if (*tmp)
+			C = tmp;
+
+	for (tmp = C; *tmp >>= 1; ++off)
+		;
+	++off;
+
+	if (off > len * sizeof (subset))
+		return FAIL;
+
+	memmove (A, A + off, off);
+
+	return SUCC;
 }
 
 Set
 setunion (Set A, Set B, size_t len)
 {
 	Set C;
+	size_t i;
+
 	C = malloc (sizeof *C * len);
 	if (!C) die ("malloc");
-	for (size_t i = 0; i < len; ++i)
-		C[i] = A[i] | B[i];
+
+	for (i = 0; i < len; ++i)
+	 	C[i] = A[i] | B[i];
+
 	return C;
 }
 
