381cfd46cf9971cb8e34d8e21118506ebfc5c923
rnoth <moth@airmail.cc>
2017-01-28 21:20:55 -0800

    cmd_insert.c: simplify, slightly

--- cmd_insert.c
+++ cmd_insert.c
@@ -5,27 +5,19 @@
 #include "edna.h"
 #include "cmd.h"
 
-extern int	insert		(State *, Buffer *, Arg *, char *);
-
 int
-insert (State *st, Buffer *buf, Arg *arg, char *error)
+cmd_insert (State *st, Buffer *buf, Arg *arg, char *error)
 {
-	Line *new;
-
 	buf->curline = (*arg->sel.v) + arg->sel.c - 1;
 
 	if (arg->mode) {
 		if (!strcmp (arg->mode, "insert")) {
-			;
+			--buf->lineno;
+			buf->curline = getprev (buf->curline);
 		} else if (!strcmp (arg->mode, "append")) {
-			if (!buf->curline->next) {
-				new = makeline();
-				linklines (buf->curline, new);
-			}
-			++buf->lineno;
-			buf->curline = buf->curline->next;
+			;
 		} else if (!strcmp (arg->mode, "change")) {
-			if (delete (st, buf, arg, error) == FAIL)
+			if (cmd_delete (st, buf, arg, error) == FAIL)
 				return FAIL;
 		} else {
 			strcpy (error, "unknown option");
