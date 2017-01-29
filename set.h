f6673bcad71ba3a96043fc82d3ae9c0e80c0b8da
rnoth <moth@airmail.cc>
2017-01-28 21:31:30 -0800

    set.h: use stdint fixed-width types

--- set.h
+++ set.h
@@ -1,22 +1,26 @@
 /* set.h -- set operations */
 #ifndef _set_
 #define _set_
+#include <stdint.h>
 
 #define freeset(A) free (A);
 #define setshiftleft(A, len, off) setshift (A, len, off, 1)
 #define setshiftright(A, len, off) setshift (A, len, off, 0)
 
-typedef unsigned long doubleset;
-typedef unsigned int  subset;
-typedef unsigned int *Set;
+typedef uint64_t  doubleset;
+typedef uint32_t  subset;
+typedef uint32_t *Set;
 
-extern Set makeset		(size_t len);
-extern Set setaddmemb		(Set A, size_t len, size_t memb);
-extern Set setcomplement	(Set A, size_t len);
-extern Set setdifference	(Set A, Set B, size_t len);
-extern Set setintersect		(Set A, Set B, size_t len);
-extern Set setshift		(Set A, size_t len, size_t off, int left);
-extern int setrightmost		(Set A, size_t len);
-extern Set setunion		(Set A, Set B, size_t len);
+extern Set	cloneset	(Set A, size_t len);
+extern size_t	offset		(uint32_t);
+extern Set	makeset		(size_t len);
+extern void*	set2vec		(Set A, size_t len);
+extern Set	setaddmemb	(Set A, size_t len, size_t memb);
+extern Set	setcomplement	(Set A, size_t len);
+extern Set	setdifference	(Set A, Set B, size_t len);
+extern Set	setintersect	(Set A, Set B, size_t len);
+extern Set	setshift	(Set A, size_t len, size_t off, int left);
+extern int	setrightmost	(Set A, size_t len);
+extern Set	setunion	(Set A, Set B, size_t len);
 
 #endif
