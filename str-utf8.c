edd966e32e8a424b6ed08e198b66b3befcf0ae5a
moth <moth@airmail.cc>
2017-02-12 10:04:26 -0500

    behave more reasonably

--- str-utf8.c
+++ str-utf8.c
@@ -7,65 +7,65 @@
 #include "util.h"
 
 int
-get_uchar (char *dest, const char *src)
+get_uchar(char *dest, const char *src)
 {
 	size_t off, len;
 	char tmp[5];
 	int ext;
 
-	ext = uchar_extent (*src);
+	ext = uchar_extent(*src);
 
 	if (ext == -1)
 		len = 1;
 	else
 		len = ext;
 
-	memset (tmp, 0, len);
-	memcpy (tmp, src, len);
+	memset(tmp, 0, len);
+	memcpy(tmp, src, len);
 	for (off = 0; off < len; ++off) {
-		if (len > 1 && isascii (tmp[off]))
+		if (len > 1 && isascii(tmp[off]))
 			goto invalid;
 		else if (((unsigned char)tmp[off]) > 0xF4)
 			goto invalid;
 
 		continue;
 	invalid:
-			ext = -ext;
+			//ext = -ext;
 			tmp[off] = 0;
 			break;
 	}
 
 	tmp[off] = 0;
 
-	memcpy (dest, tmp, off + 1);
+	memcpy(dest, tmp, off + 1);
 
-	return (ext);
+	return off;
 }
 
 int
-uchar_extent (const unsigned char ch)
+uchar_extent(const unsigned char ch)
 {
 	if (!ch)
-		return (0);
-	else if (isascii (ch))
-		return (1);
+		return 0;
+	else if (isascii(ch))
+		return 1;
 	else if (ch >= 0xC2 && ch <= 0xDF)
-		return (2);
+		return 2;
 	else if (ch >= 0xE0 && ch <= 0xEF)
-		return (3);
+		return 3;
 	else if (ch >= 0xF0 && ch <= 0xF4)
-		return (4);
+		return 4;
 	else
-		return (-1); /* error */
+		return -1; /* error */
 } 
 
 size_t
-ustrlen (const char *s)
+ustrlen(const char *s)
 {
 	size_t ret = 0;
 
 	while (*s && ++ret)
-		s += uchar_extent (*s);
+		s += uchar_extent(*s);
 
-	return (ret);
+	return ret;
 }
