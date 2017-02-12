8b33d69df73bee91636a6dea98b5fe455795f8a2
moth <moth@airmail.cc>
2017-02-12 09:01:00 -0500

    more buffer operations

--- buffer.h
+++ buffer.h
@@ -6,6 +6,7 @@
 
 #include "edna.h"
 #include "state.h"
+#include "str.h"
 
 enum {
 	BUF_SET,
@@ -16,29 +17,33 @@ enum {
 struct Buffer {
 	/* file info */
 	int	dirty;
-	FILE*	file;
-	char*	filename;
+	FILE	*file;
+	String	*name;
 
 	/* line info */
 	size_t	len;
-	Line*	top;
-	Line*	bot;
-	Line*	cur;
 	size_t	pos;
+	Line	*top;
+	Line	*bot;
+	Line	*cur;
 
 	/* misc. info */
-	Mode*	mode;
+	Mode	*mode;
 };
 
 /* buffer.c */
 extern int	addline		(Buffer *, Line *);	
-extern Line*	buftell		(Buffer *);
+extern int	bufname		(Buffer *, char *);
+extern int	bufopen		(Buffer *, char *);
 extern int	bufseek		(Buffer *, int, long);
+extern Line*	buftell		(Buffer *);
 extern Buffer*	makebuf		(void);
 extern void	freebuf		(Buffer *);
 extern int	initbuf		(Buffer *, char *);
+extern void	killbuf		(Buffer *);
 extern int	rmcurline	(Buffer *);
 extern int	rmline		(Buffer *, Line *);
+extern int	setfilename	(Buffer *, char *fn);
 
 /* file.c */
 extern int	readbuf		(Buffer *, char *);
