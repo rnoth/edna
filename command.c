defc48d45c856cb445af80ad82bc6ec64c258eba
moth <moth@airmail.cc>
2017-02-13 19:54:17 -0500

    style changes

--- command.c
+++ command.c
@@ -16,21 +16,21 @@
 static int	runcmd (State *, Buffer *, Command *, Arg *, char *err);
 
 int
-grabline (State *st, Buffer *buf, String *s, char *err)
+grabline(State *st, Buffer *buf, String *s, char *err)
 {
 	int ret = SUCC;
 
 	errno = 0;
-	if (FAIL == readline (s, stdin)) {
-		if (feof (stdin))
-			strcpy (err, "quit");
+	if (readline(s, stdin) == FAIL) {
+		if (feof(stdin))
+			strcpy(err, "quit");
 		else
-			strncpy (err, strerror (errno), 20);
+			strcpy(err, strerror (errno));
 		ret = FAIL;
 		clearerr (stdin);
 	}
 
-	return (ret);
+	return ret;
 
 }
 
@@ -39,17 +39,13 @@ freearg (Arg *arg)
 {
 	size_t i;
 
-	if (arg->param.c) {
-		for (i = 0;i < arg->param.c; ++i)
-			free (arg->param.v[arg->cnt]);
-		free_vector (arg->param);
-	}
-	if (arg->name)
-		free (arg->name);
-	if (arg->mode)
-		free (arg->mode);
-	free_vector (arg->sel);
-	free (arg);
+	for (i = 0; i < arg->param.c; ++i)
+		free(arg->param.v[arg->cnt]);
+	free_vector(arg->param);
+	if (arg->name) free(arg->name);
+	if (arg->mode) free(arg->mode);
+	free_vector(arg->sel);
+	free(arg);
 
 }
 
@@ -57,8 +53,8 @@ Arg *
 makearg (void)
 {
 	Arg *ret;
-	ret = calloc (1, sizeof *ret);
-	make_vector (ret->sel);
+	ret = calloc(1, sizeof *ret);
+	make_vector(ret->sel);
 	return ret;
 }
 
@@ -69,41 +65,44 @@ runcmd (State *st, Buffer *buf, Command *cmd, Arg *arg, char *err)
 	size_t pos = 0;
 	Selection *sel;
 	String *addr;
+
 	if (!cmd) {
-		strcpy (err, "unknown command");
+		strcpy(err, "unknown command");
 		goto fail;
 	}
 
 	if (cmd->mode) {
-		arg->mode = malloc (strlen (cmd->mode) + 1 * sizeof *arg->mode);
+		arg->mode = malloc(strlen(cmd->mode) + 1 * sizeof *arg->mode);
 		if (!arg->mode) die("malloc");
-		strcpy (arg->mode, cmd->mode);
+		strcpy(arg->mode, cmd->mode);
 	}
 
 	if (arg->sel.c == 0 && cmd->defaddr) {
-		addr = chartostr (cmd->defaddr);
+		addr = chartostr(cmd->defaddr);
 
-		sel = getaddr (addr, &pos, buf, err);
+		sel = getaddr(addr, &pos, buf, err);
+		freestring(addr);
 		if (sel == NULL)
 			goto fail;
 
-		make_vector (arg->sel);
-		vec_copy (arg->sel, *sel);
+		free_vector(arg->sel);
+		make_vector(arg->sel);
+		vec_copy(arg->sel, *sel);
 
-		free_vector (*sel);
-		free (sel);
+		free_vector(*sel);
+		free(sel);
 	}
 
-	return (cmd->func (st, buf, arg, err));
+	return cmd->func(st, buf, arg, err);
 
 fail:
-	return (FAIL);
+	return FAIL;
 }
 
 int
 cmdchck (const void *a, const void *b)
 {
-	return strcmp (((char *)a), ((Command *)b)->name);
+	return strcmp(((char *)a), ((Command *)b)->name);
 }
 
 int
@@ -120,18 +119,17 @@ cmdeval (State *st, Buffer *buf, String *s, char *err)
 	Command *cmd;
 
 	ret = FAIL;
-	arg = makearg ();
+	arg = makearg();
 
-	if (parseline (s, buf, arg, err) == FAIL) goto finally;
+	if (parseline(s, buf, arg, err) == FAIL) goto finally;
 
-	cmd = bsearch (arg->name, st->cmds.v, st->cmds.c,
+	cmd = bsearch(arg->name, st->cmds.v, st->cmds.c,
 			sizeof *st->cmds.v, &cmdchck);
 
-	if (runcmd (st, buf, cmd, arg, err) == SUCC)
-		ret = SUCC;
+	if (runcmd(st, buf, cmd, arg, err) == SUCC) ret = SUCC;
 
 finally:
-	freearg (arg);
+	freearg(arg);
 	return ret;
 
 }
