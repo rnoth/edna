6261834300b859622ea6ac699a323e08d8d7b05c
moth <moth@airmail.cc>
2017-02-13 19:52:23 -0500

    style changes

--- addr-op.c
+++ addr-op.c
@@ -8,49 +8,50 @@
 #include "set.h"
 
 Set *
-addr_plus (Node *left, Node *right, Buffer *buf, char *err)
+addr_plus(Node *left, Node *right, Buffer *buf, char *err)
 {
 	Set *ret = NULL, *inc = NULL;
 	size_t off;
 
 	if (left)
-		ret = evaltree (left, buf, err);
+		ret = evaltree(left, buf, err);
 
 	else	/* default to current line */
-		ret = addr_dot (left, buf, err);
+		ret = addr_dot(left, buf, err);
 
 	if (ret == NULL)
 		goto fail;
 
 	if (right) {
 
-		inc = evaltree (right, buf, err);
+		inc = evaltree(right, buf, err);
 
 		if (inc == NULL)
 			goto fail;
 
-		off = setrightmost (inc) - 1;
+		off = setrightmost(inc) - 1;
 
-		if (setshiftleft (ret, off) == NULL)
+		if (setshiftleft(ret, off) == NULL)
 			goto fail;
 
 	} else {
 
-		inc = make_set ();
+		inc = make_set();
 
 		off = 1;
 
-		if (setshiftleft (ret, off) == NULL)
+		if (setshiftleft(ret, off) == NULL)
 			goto fail;
 
 	}
 
-	return (ret);
+	free_set(inc);
+	return ret;
 
 fail:
-	strcpy (err, "invalid line address");
-	free_set (ret);
-	free_set (inc);
+	strcpy(err, "invalid line address");
+	free_set(ret);
+	free_set(inc);
 	return NULL;
 	
 }
