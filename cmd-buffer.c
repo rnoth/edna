1e01961a47c606ec1d6f48e8c8489417c3542ddb
moth <moth@airmail.cc>
2017-02-14 18:52:33 -0500

    small re-order

--- cmd-buffer.c
+++ cmd-buffer.c
@@ -7,6 +7,29 @@
 #include "state.h"
 
 int
+cmd_openbuf(State *st, Buffer buf, Arg *arg, char *error)
+{
+	Buffer tmp;
+	size_t i;
+
+	if (!arg->param.c) {
+		strcpy(error, "no filenames provided");
+		return FAIL;
+	}
+
+	i = 0;
+	do {
+		tmp = makebuf();
+		initbuf(tmp, arg->param.v[i]);
+		readbuf(tmp, error);
+		bufclean(buf);
+		addbuf(st, tmp);
+	} while (++i < arg->param.c);
+
+	return SUCC;
+}
+
+int
 cmd_switchbuf(State *st, Buffer buf, Arg *arg, char *error)
 {
 	if (!arg->mode) {
@@ -28,27 +51,4 @@ cmd_switchbuf(State *st, Buffer buf, Arg *arg, char *error)
 
 	strcpy (error, "unknown option");
 	return FAIL;
-}
-
-int
-cmd_openbuf(State *st, Buffer buf, Arg *arg, char *error)
-{
-	Buffer tmp;
-	size_t i;
-
-	if (!arg->param.c) {
-		strcpy(error, "no filenames provided");
-		return FAIL;
-	}
-
-	i = 0;
-	do {
-		tmp = makebuf();
-		initbuf(tmp, arg->param.v[i]);
-		readbuf(tmp, error);
-		bufclean(buf);
-		addbuf(st, tmp);
-	} while (++i < arg->param.c);
-
-	return SUCC;
 }
