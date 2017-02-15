2b651d77518eae09f3a85284840550939830e500
moth <moth@airmail.cc>
2017-02-14 19:09:58 -0500

    finally add the organiation

--- cmd.h
+++ cmd.h
@@ -37,18 +37,25 @@ extern int	insprompt	(State *, Buffer, String *, char *);
 /* parse.c */
 extern int	parseline	(String *, Buffer, Arg *, char *);
 
-/* TODO: organization */
-extern int	cmd_delete	(State *, Buffer, Arg *, char *);
+/* cmd-buffer.c */
+extern int	cmd_listbuf	(State *, Buffer, Arg *, char *);
+extern int	cmd_openbuf	(State *, Buffer, Arg *, char *);
+extern int	cmd_switchbuf	(State *, Buffer, Arg *, char *);
+
+/* cmd-file.c */
 extern int	cmd_edit	(State *, Buffer, Arg *, char *);
 extern int	cmd_filename	(State *, Buffer, Arg *, char *);
-extern int	cmd_gotol	(State *, Buffer, Arg *, char *);
-extern int	cmd_help	(State *, Buffer, Arg *, char *);
-extern int	cmd_insert	(State *, Buffer, Arg *, char *);
-extern int	cmd_nop		(State *, Buffer, Arg *, char *);
-extern int	cmd_openbuf	(State *, Buffer, Arg *, char *);
-extern int	cmd_print	(State *, Buffer, Arg *, char *);
 extern int	cmd_quit	(State *, Buffer, Arg *, char *);
-extern int	cmd_subst	(State *, Buffer, Arg *, char *);
-extern int	cmd_switchbuf	(State *, Buffer, Arg *, char *);
 extern int	cmd_write	(State *, Buffer, Arg *, char *);
+
+/* cmd-insert.c */
+extern int	cmd_insert	(State *, Buffer, Arg *, char *);
+
+/* cmd-print.c */
+extern int	cmd_help	(State *, Buffer, Arg *, char *);
+extern int	cmd_print	(State *, Buffer, Arg *, char *);
+
+/* cmd-register.c */
+extern int	cmd_delete	(State *, Buffer, Arg *, char *);
+
 #endif
