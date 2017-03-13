5789f90a3c8171078037a5f00380b553bde308b9
moth <moth@airmail.cc>
2017-03-12 21:06:45 -0400

    no more string-specific functions

--- str.c
+++ str.c
@@ -4,54 +4,24 @@
 #include "str.h"
 #include "util.h"
 
-bool
-expandstring(String *str)
+void
+chomp(char *s)
 {
-	char *tmp;
-
-	tmp = realloc(str->v, str->m * 2);
-	if (!tmp) {
-		warn("realloc");
-		return false;
-	}
-	str->v = tmp;
-	str->m *= 2;
-	return true;
-}
-
-bool
-copystring(String *dest, String const *src)
-{
-	if (src->b > dest->m)
-		if (!expandstring(dest)) return false;
-	memset(dest->v, 0, dest->m);
-	memcpy(dest->v, src->v, src->c);
-	dest->c = src->c;
-	return true;
+	char *nl;
+	do if (*s == '\n') nl = s; while (*s++);
+	*nl = 0;
 }
 
 void
 freestring(String *str)
 {
-	if (str == NULL) return;
-	free(str->v);
-	memset(str, 0, sizeof *str);
-	free(str);
+	vec_free(str);
 }
 
 String *
 makestring(void)
 {
 	String *ret;
-
-	ret = calloc(1, sizeof *ret);
-	if (!ret) die("calloc");
-
-	ret->v = calloc(80, 1);
-	if (!ret->v) die("calloc");
-
-	ret->m = 80;
-	ret->b = 1;
-
+	make_vector(ret);
 	return ret;
 }
