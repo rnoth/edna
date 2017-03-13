749d2ceaedc344e14ee3fdd49117362b57bd2794
moth <moth@airmail.cc>
2017-03-12 21:05:10 -0400

    move str-io.c into input.c

--- input.c
+++ input.c
@@ -6,18 +6,34 @@
 int
 grabline(State *st, Buffer *buf, String *s, char *err)
 {
-	int ret = SUCC;
+	int ret = 0;
 
 	errno = 0;
-	if (!readline(s, stdin)) {
+	if (readline(s, stdin)) {
 		if (feof(stdin)) strcpy(err, "quit");
-		else strcpy(err, strerror (errno));
+		else strcpy(err, strerror(errno));
 
-		ret = FAIL;
-		clearerr (stdin);
+		ret = errno;
+		clearerr(stdin);
 	}
 
 	return ret;
 
 }
 
+int
+readline(String *s, FILE *f)
+{
+	int ch;
+
+	vec_truncate(s, 0);
+	for (;;) {
+		ch = fgetc(f);
+		if (ch == EOF) {
+			if (feof(f)) return -1;
+			else return errno;
+		}
+		vec_append(s, &ch);
+		if (ch == '\n') return 0;
+	}
+}
