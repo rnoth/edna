ce17fac80901060f7738f65c58c2c938162f6bcd
moth <moth@airmail.cc>
2017-04-01 09:56:19 -0400

    examples(): fix INS_PROMPT, add prototypes

--- examples/config.h
+++ examples/config.h
@@ -6,11 +6,14 @@
 
 /* advanced examples */
 #define PROMPT		"%ld:", bufgetpos(buf)			/* have line numbers in prompt */
-#define INS_PROMPT	"%ld ", bufgetpos(buf)			/* have line number in ins mode */
+#define INS_PROMPT	"%ld ", bufgetpos(buf) + 1		/* have line number in ins mode */
 #define PRINT_FMT	"%ld|%s", bufgetpos(buf),arr(arr(arg->sel)[0]->str)	/* like ed's 'n' */
 #define ERROR		"%s\n", err				/* like ed's 'H', without the '?' */
 
 extern const Mode modes[];
 extern const Command commands[];
+
+size_t sizeof_modes(void);
+size_t sizeof_commands(void);
 
 #endif
